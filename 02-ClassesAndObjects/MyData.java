45f41dfa9e431b808fd264b8e529a5945b326e42
Robert1510c <robert1510c@gmail.com>
2023-10-17 00:00:41 +0200

    First changes

---
+++ 02-ClassesAndObjects/MyData.java
@@ -0,0 +1,20 @@
+public class MyData {
+    public static void main(String[] args) {
+        String name = "Robert";
+        String surname = "Ciepliński";
+        int age = 20;
+        String studentId = "227572";
+        boolean FirstYear = false;
+        String FieldOfStudy = "IT";
+        String UniversityName = "Uniwersytet ekonomiczny w Krakowie";
+
+
+        System.out.println("Name: "+ name);
+        System.out.println("Surname: "+ surname);
+        System.out.println("Age: "+ age);
+        System.out.println("StudentID: "+ studentId);
+        System.out.println("First Year Student?- "+ FirstYear);
+        System.out.println("Field of Study: "+ FieldOfStudy);
+        System.out.println("University: "+ UniversityName);
+    }
+}
